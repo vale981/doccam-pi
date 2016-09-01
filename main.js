5cb974b6ad41e2a5f1b2ee1f31cff0def7b5c9c9
Hiro Protagonist <valentin@boettcher.cf>
2016-09-01 23:28:19 +1200

    final2

--- main.js
+++ main.js
@@ -20,9 +20,9 @@ let status = {
 }
 
 let errors = ['Camera Disconnected', 'YoutTube Disconnected', 'Wrong ffmpeg executable.'];
-let cmd;
+let cmd, sshpid;
 
-let spawn = function () {
+let spawn = function() {
     source = 'rtsp://' + config.camIP + ':' + config.camPort + '/' + config.camProfile;
     ffmpeg.setFfmpegPath(config.ffmpegPath)
     delete cmd;
@@ -36,21 +36,21 @@ let spawn = function () {
         .audioChannels(1)
         .audioFrequency(11025)
         .audioCodec('libmp3lame')
-        .on('start', function (commandLine) {
+        .on('start', function(commandLine) {
             status.running = 0;
             wLog('Spawned Ffmpeg with command: ' + commandLine, 4);
         })
-        .on('end', function (o, e) {
+        .on('end', function(o, e) {
             imDead('Normal Stop.', e);
         })
-        .on('error', function (err, o, e) {
+        .on('error', function(err, o, e) {
             console.log(err);
             if (err.message.indexOf(source) > -1)
                 criticalProblem(0, handleDisc, config.camIP, config.camPort)
             else if (err.message.indexOf(source + 'Input/output error') > -1 || err.message.indexOf('rtmp://a.rtmp.youtube.com/live2/' + config.key) > -1)
                 criticalProblem(1, handleDisc, 'a.rtmp.youtube.com/live2/', 1935);
             else if (err.message.indexOf('spawn') > -1 || err.message.indexOf('niceness') > -1)
-                criticalProblem(2, function () {});
+                criticalProblem(2, function() {});
             else if (err.message.indexOf('SIGKILL') > -1)
                 imDead('Normal Stop.', e);
             else
@@ -70,20 +70,20 @@ let spawn = function () {
     cmd.run();
 }
 
-let getSnap = function (cb) {
+let getSnap = function(cb) {
     snapSource = 'rtsp://' + config.camIP + ':' + config.camPort + '/' + config.snapProfile;
     let picBuff = new WMStrm();
     recCmd = ffmpeg(snapSource)
-        .on('start', function (commandLine) {
+        .on('start', function(commandLine) {
             wLog('Snapshot ' + commandLine, 4);
         })
-        .on('error', function (err, o, e) {})
+        .on('error', function(err, o, e) {})
         .outputFormat('mjpeg')
         .frames(1)
         .stream(picBuff, {
             end: true
         });
-    picBuff.on('finish', function () {
+    picBuff.on('finish', function() {
         try {
             cb(picBuff.memStore.toString('base64'));
             delete pickBuff;
@@ -107,7 +107,7 @@ function imDead(why, e = '') {
     }
     if (!mustBe) {
         wLog('Crash! ' + why + ' ' + e, 2);
-        setTimeout(function () {
+        setTimeout(function() {
             spawn();
         }, 1000);
     }
@@ -115,7 +115,7 @@ function imDead(why, e = '') {
 }
 
 function criticalProblem(err, handler, ...args) {
-    setTimeout(function () {
+    setTimeout(function() {
         status.running = 2
         status.error = err
         wLog('Critical Problem: ' + errors[err], 3);
@@ -134,10 +134,9 @@ function handleDisc(info) {
     let [host, port] = info
     isReachable(host, port, is => {
         if (is) {
-            //console.log('is');
-            spawn()
+            spawn();
         } else {
-            setTimeout(function () {
+            setTimeout(function() {
                 handleDisc(info)
             }, 10000);
         }
@@ -148,9 +147,9 @@ function isReachable(host, port, callback) {
     http.get({
         host: host.split('/')[0],
         port: port
-    }, function (res) {
+    }, function(res) {
         callback(true);
-    }).on("error", function (e) {
+    }).on("error", function(e) {
         if (e.message == "socket hang up")
             callback(true)
         else
@@ -160,7 +159,7 @@ function isReachable(host, port, callback) {
 
 var commandHandlers = function commandHandlers(command, cb) {
     var handlers = {
-        startStop: function () {
+        startStop: function() {
             if (status.running !== 2)
                 if (status.running === 0) {
                     wLog("Stop Command!", 1);
@@ -171,7 +170,7 @@ var commandHandlers = function commandHandlers(command, cb) {
                     spawn();
                 }
         },
-        snap: function () {
+        snap: function() {
             getSnap(snap => {
                 socket.emit('data', {
                     type: 'snap',
@@ -179,13 +178,13 @@ var commandHandlers = function commandHandlers(command, cb) {
                 }, command.sender);
             });
         },
-        config: function () {
+        config: function() {
             socket.emit('data', {
                 type: 'config',
                 data: config,
             }, command.sender);
         },
-        changeSettings: function () {
+        changeSettings: function() {
             for (let set in command.data) {
                 if (config[set])
                     config[set] = command.data[set];
@@ -194,7 +193,7 @@ var commandHandlers = function commandHandlers(command, cb) {
             if (config.configured === true)
                 oldConfigured = true;
             config.configured = true;
-            fs.writeFile('./config.js', JSON.stringify(config, undefined, 2), function (err) {
+            fs.writeFile('./config.js', JSON.stringify(config, undefined, 2), function(err) {
                 if (err) {
                     socket.emit('data', {
                         type: 'message',
@@ -229,7 +228,7 @@ var commandHandlers = function commandHandlers(command, cb) {
                 }
             });
         },
-        restart: function () {
+        restart: function() {
             if (status.running === 0) {
                 wLog("Restart Command!", 1);
                 mustBe = true;
@@ -240,8 +239,8 @@ var commandHandlers = function commandHandlers(command, cb) {
                 spawn();
             }
         },
-        getLogs: function () {
-            fs.readFile(config.logPath, 'utf-8', function (err, data) {
+        getLogs: function() {
+            fs.readFile(config.logPath, 'utf-8', function(err, data) {
                 if (err) throw err;
 
                 let lines = data.trim().split('$end$').slice(-100);
@@ -263,7 +262,7 @@ var commandHandlers = function commandHandlers(command, cb) {
 }
 
 function wLog(msg, l = 0) {
-    fs.appendFile(config.logPath, Date() + '$$$(i' + l + ')' + msg + '$end$\n', function (err) {
+    fs.appendFile(config.logPath, Date() + '$$$(i' + l + ')' + msg + '$end$\n', function(err) {
         if (err)
             throw new Error('Can\'t write Log! ', err);
     });
@@ -277,7 +276,7 @@ function handleKill() {
     process.exit();
 }
 
-process.on('SIGTERM', function () {
+process.on('SIGTERM', function() {
     handleKill();
 });
 // process.on('SIGKILL', function () {
@@ -303,100 +302,95 @@ function init() {
 }
 
 
+let d = false;
 
+function initSSH(cb) {
+    status.ssh = {
+        user: config['ssh-user'],
+        localUser: config['ssh-local-user'],
+        masterPort: config['ssh-port']
+    };
 
-function initSocket() {
-    function connectSSH(cb) {
-        if (!config['ssh-key']) {
-            cb();
-        } else {
-            if (!cb)
-                cb = function () {
-                    socket.emit('change', {
-                        change: {
-                            ssh: {
-                                port: status.ssh.port
-                            }
-                        }
-                    });
-                };
-            status.ssh = {
-                user: config['ssh-user'],
-                localUser: config['ssh-local-user'],
-                masterPort: config['ssh-port']
-            };
+    checkSSH((err) => {
+        if (err)
+            throw err;
+        connectSSH(cb);
+    });
+}
 
-            function connect() {
-                let ssh = spawnP(`ssh -p ${config['ssh-port']} -f -N -R ${status.ssh.port}:localhost:22 ${config['ssh-user']}@${config.master.replace(/(http\:\/{2}|\:[0-9]+)/g, '')}`, {
-                    detached: true,
-                    shell: true
-                });
-
-                ssh.on('close', () => {
-                    connectSSH();
-                });
-
-                ssh.on('error', () => {
-                    connectSSH();
-                });
-
-                cb();
+function connectSSH(cb = function() {
+    socket.emit('change', {
+        change: {
+            ssh: {
+                port: status.ssh.port,
+                camForwardPort: status.ssh.camForwardPort
             }
-            exec('ps -ax | grep ssh', (error, stdout, stderr) => {
-                let re = /\s*([0-9]+).*\s([0-9]+)\:localhost/g;
-                let m, pid, port;
-                pid = [];
-                while ((m = re.exec(stdout)) !== null) {
-                    if (m.index === re.lastIndex) {
-                        re.lastIndex++;
-                    }
-                    pid.push(m[1]);
-                    port = parseInt(m[2]);
-                }
-                if (pid.length > 0 && port) {
-                    status.ssh.port = port;
-                    exec(`kill ${pid.join(' ')}`, err => {
-                        if (error)
-                            cb(err);
-                        else
-                            connect();
-                    });
-                } else {
-                    socket.emit('getSSHPort', port => {
-                        status.ssh.port = port;
-                        connect();
-                    });
-                }
+        }
+    });
+}) {
+    socket.emit('getSSHPort', ports => {
+        [status.ssh.port, status.ssh.camForwardPort] = ports;
+        let ssh = exec(`ssh -p ${config['ssh-port']} -f -N -R ${status.ssh.port}:localhost:22 -L \*:${status.ssh.camForwardPort}:${config.camIP}:80 ${config['ssh-user']}@${config.master.replace(/(http\:\/{2}|\:[0-9]+)/g, '')} && pidof ssh`, {
+            detached: false,
+            env: '/usr/local/sbin:/usr/local/bin:/usr/bin:/bin',
+            shell: false
+        });
+        ssh.stdout.on('data', (out) => {
+            sshPid = parseInt(out.split(' ')[0]);
+            fs.writeFile('./ssh.pid', sshPid);
+        });
+        ssh.on('error', () => {
+            connectSSH();
+        });
+        ssh.on('close', () => {
+            initSSH();
+        });
+        cb();
+    });
+}
+
+function checkSSH(cb) {
+    exec('ps -ax | grep ssh', (error, stdout, stderr) => {
+        let m, pid, alive;
+        let re = /([0-9]+).*-p\s[0-9]+\s-f\s-N\s-R\s[0-9]+:.*?:[0-9]+\s-L\s\*:[0-9]+:.*?:[0-9]+\s/g;
+        pid = [];
+        while ((m = re.exec(stdout)) !== null) {
+            if (m.index === re.lastIndex) {
+                re.lastIndex++;
+            }
+            pid.push(m[1]);
+        }
+        if (pid.length > 0) {
+            exec(`kill ${pid.join(' ')}`, err => {
+                if (error)
+                    cb(true);
             });
         }
-    }
-    socket.on('connect', function () {
+        if (!alive) {
+            cb(false);
+        }
+    });
+}
+
+function initSocket() {
+    socket.on('connect', function() {
         wLog('Connected to Master: ' + config.master + '.');
-        connectSSH(err => {
-            if (err)
-                throw err;
+        if (config['ssh-user'])
+            initSSH(err => {
+                if (err)
+                    throw err;
+                socket.emit('meta', status);
+            });
+        else {
             socket.emit('meta', status);
-        });
+        }
     });
 
-    socket.on('disconnect', function () {
+    socket.on('disconnect', function() {
+        d = true;
         socket.disconnect();
         init();
     });
-
-    ss(socket).on('getPanel', function (req, res) {
-        if (status.error !== 0)
-            try {
-                request[req.method.toLowerCase()](`http://${config.camUsername}:${config.camPassword}@${config.camIP}/${req.path}`).on('error', () => {
-                    res.end();
-                }).pipe(res)
-            } catch (e) {
-                res.end();
-            }
-        else {
-            res.end();
-        }
-    })
 
     socket.on('command', (command, cb) => {
         commandHandlers(command, cb);
@@ -407,4 +401,4 @@ function readConfig() {
     return JSON.parse(fs.readFileSync('./config.js'));
 }
 
-init();;
+init();
